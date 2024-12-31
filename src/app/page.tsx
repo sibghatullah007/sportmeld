5930fad0eb8beeaec5e6dc089136476364a22a24
Sibgha Tullah <sibghazahid007@gmailcom>
2025-01-01 04:00:17 +0500

    Image Position Bottom Value set

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,7 +18,7 @@ export default function Home() {
       <div className="relative w-full flex items-center justify-center">
         <Image
           src="/gradient_home_left.webp"
-          className="absolute -bottom-[20] w-[30%] left-0 object-cover z-0"
+          className="absolute bottom-[20] w-[30%] left-0 object-cover z-0"
           alt="left"
           layout="intrinsic"
           width={500}
@@ -28,7 +28,7 @@ export default function Home() {
         <Image src={'sample_dashboard.svg'} height={600} width={800} alt="Sample Dashboard" className="w-[90%] z-20 relative" draggable={false} />
         <Image
           src="/gradient_home_right.webp"
-          className="absolute -bottom-[20] w-[30%] right-0 object-cover z-0"
+          className="absolute bottom-[20] w-[30%] right-0 object-cover z-0"
           alt="right"
           layout="intrinsic"
           width={500}
